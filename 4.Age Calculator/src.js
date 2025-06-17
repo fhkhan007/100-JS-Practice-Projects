c7a1004b510b6b127c9840c79ff9e6f774317b56
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-06-17 21:34:13 +0600

    calculate age

---
+++ 4.Age Calculator/src.js
@@ -0,0 +1,20 @@
+const birthdayEl = document.getElementById("birthday");
+const btnEl = document.getElementById("btn");
+const resultEl = document.getElementById("result");
+
+
+function calculateAge(){
+    const birthdayValue = birthdayEl.value;
+
+    if(birthdayValue === "") {
+        alert('Please enter your birthday')
+    }
+    else {
+        const age = getAge(birthdayValue);
+        resultEl.innerText = `Your age is ${age} ${age === 1 ? "year" : "years"} old`;
+    }
+}
+
+function getAge(){
+    
+}
